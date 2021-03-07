9c4cc303e9a14cc27e26c1d59d317d4c950807f5
crmviet <ng.manhtuong@gmail.com>
2021-03-07 18:20:10 +0700

    Assignment 1

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const hostname = 'localhost'; 
 const port = 3000;
 
@@ -12,6 +15,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/dishes/:dishId', dishRouter);
+
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId', promoRouter);
+
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:leaderId', leaderRouter);
+
 app.use(express.static(__dirname+'/public'));
 
 app.use((req, res, next) => {
@@ -19,31 +30,6 @@ app.use((req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 }); 
-
-
-   // Param ID
-
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: '
-        + req.params.dishId + ' to you!');
-});
-    
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes ' 
-        + req.params.dishId );
-});
-
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId +'\n');
-    res.end('Will update the dish: ' + req.body.name 
-        + ' with details: ' + req.body.description); 
-});
-
-app.delete('/dishes/:dishId', (req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
 
 const server = http.createServer(app);
 
